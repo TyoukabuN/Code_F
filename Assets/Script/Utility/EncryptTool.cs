be1f86e9184fdbea56313dbe25cb2fc39b7b17a0
TyoukabuN <40262830+TyoukabuN@users.noreply.github.com>
2018-06-29 20:04:48 +0800

    0629

---
+++ Assets/Script/Utility/EncryptTool.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+namespace Tyou
+{
+    public static class EncryptTool
+    {
+        public static void Encrypt()
+        {
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                using (BinaryWriter m)
+            }
+        }
+    }
+}
